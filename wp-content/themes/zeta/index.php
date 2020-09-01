e6baaa2631f611bffb99cc691b65d21751b6b332
Bruno Serrão <brunosserrao@gmail.com>
2020-09-01 12:54:45 +0100

    removendo formulario de contato

--- wp-content/themes/zeta/index.php
+++ wp-content/themes/zeta/index.php
@@ -137,9 +137,9 @@
 					</ul>
 				</div>
 
-				<div class="flex-1 texto-branco">
-					<?php echo do_shortcode('[contact-form-7 id="49" title="Formulário de contato"]') ?>	
-				</div>
+				<!-- <div class="flex-1 texto-branco">
+					<?php // echo do_shortcode('[contact-form-7 id="49" title="Formulário de contato"]') ?>	
+				</div> -->
 			</div>
 		</div>
 	</div>
